201affd1667643a94e6a1e75e8c3fb1d8fac9d88
Tobechi Duru <tobechi.duru001@gmail.com>
2024-04-24 15:28:20 +0100

    Updates

---
+++ FAQs/dropdown.js
@@ -0,0 +1,95 @@
+function showContent(){
+  const content = document.querySelector('.js-content');
+  const minus = document.querySelector('.js-minus');
+  const plus = document.querySelector('.js-plus');
+
+  if (content.style.display === 'none'){
+    content.style.display = 'flex';
+    minus.style.display = 'flex';
+    plus.style.displsy = 'none';
+  }
+}
+
+function closeContent(){
+  const content = document.querySelector('.js-content');
+  const minus = document.querySelector('.js-minus');
+  const plus = document.querySelector('.js-plus');
+  
+  if (content.style.display === 'flex'){
+    content.style.display = 'none';
+    minus.style.display = 'none';
+    plus.style.display = 'flex';
+  }
+}
+
+function secondShowContent(){
+  const content = document.querySelector('.js-secondContent');
+  const minus = document.querySelector('.js-secondMinus');
+  const plus = document.querySelector('.js-secondPlus');
+
+  if (content.style.display === 'none'){
+    content.style.display = 'flex';
+    minus.style.display = 'flex';
+    plus.style.displsy = 'none';
+  }
+}
+
+function secondCloseContent(){
+  const content = document.querySelector('.js-secondContent');
+  const minus = document.querySelector('.js-secondMinus');
+  const plus = document.querySelector('.js-secondPlus');
+  
+  if (content.style.display === 'flex'){
+    content.style.display = 'none';
+    minus.style.display = 'none';
+    plus.style.display = 'flex';
+  }
+}
+
+function thirdShowContent(){
+  const content = document.querySelector('.js-thirdContent');
+  const minus = document.querySelector('.js-thirdMinus');
+  const plus = document.querySelector('.js-thirdPlus');
+
+  if (content.style.display === 'none'){
+    content.style.display = 'flex';
+    minus.style.display = 'flex';
+    plus.style.displsy = 'none';
+  }
+}
+
+function thirdCloseContent(){
+  const content = document.querySelector('.js-thirdContent');
+  const minus = document.querySelector('.js-thirdMinus');
+  const plus = document.querySelector('.js-thirdPlus');
+  
+  if (content.style.display === 'flex'){
+    content.style.display = 'none';
+    minus.style.display = 'none';
+    plus.style.display = 'flex';
+  }
+}
+
+function fourthShowContent(){
+  const content = document.querySelector('.js-fourthContent');
+  const minus = document.querySelector('.js-fourthMinus');
+  const plus = document.querySelector('.js-fourthPlus');
+
+  if (content.style.display === 'none'){
+    content.style.display = 'flex';
+    minus.style.display = 'flex';
+    plus.style.displsy = 'none';
+  }
+}
+
+function fourthCloseContent(){
+  const content = document.querySelector('.js-fourthContent');
+  const minus = document.querySelector('.js-fourthMinus');
+  const plus = document.querySelector('.js-fourthMlus');
+  
+  if (content.style.display === 'flex'){
+    content.style.display = 'none';
+    minus.style.display = 'none';
+    plus.style.display = 'flex';
+  }
+}
